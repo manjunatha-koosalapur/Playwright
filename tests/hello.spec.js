6854a60138baf608afcb57e164692488aed44191
manjunatha-koosalapur <manjunatha.koosalapur@gmail,com>
2022-09-02 22:00:26 +0530

    firstCommit

---
+++ tests/hello.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test')
+const {hello, helloworld} = require('./demo/hello')
+// import {hello, helloworld} from './demo/hello'
+// console.log (hello());
+// console.log (helloworld());
+
+test('Hello World', ()=>{
+    console.log (hello());
+    console.log (helloworld());
+})
---
+++ tests/record_demo.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Record demo test', async ({ page }) => {
+
+  // Go to https://www.saucedemo.com/
+  await page.goto('https://www.saucedemo.com/');
+
+  // Click [data-test="username"]
+  await page.locator('[data-test="username"]').click();
+
+  // Fill [data-test="username"]
+  await page.locator('[data-test="username"]').fill('standard_user');
+
+  // Click [data-test="password"]
+  await page.locator('[data-test="password"]').click();
+
+  // Fill [data-test="password"]
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+
+  // Click [data-test="login-button"]
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+  // Click text=Open Menu
+  await page.locator('text=Open Menu').click();
+
+  // Click text=Logout
+  await page.locator('text=Logout').click();
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+});
